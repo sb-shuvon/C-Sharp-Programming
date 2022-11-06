2028919a9e52ba568e12982928fff1b299e74d22
sb shuvon <sbshuvon07@gmail.com>
2022-11-05 19:51:30 -0600

    c sharp

---
+++ Array Accending and Decending order/Array Accending and Decending order/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+namespace Array_Sorted 
+{
+    class program
+    {
+        static void Main(string[] args)
+        {
+            int i, j,temp;
+            int[] array = new int[5];
+
+            Console.WriteLine("Enter 5 Element: ");
+            for (i = 0; i < 5; i++)
+            {
+                 array[i] =Convert.ToInt32(Console.ReadLine());
+            }
+            
+            for (i = 0; i < 5; i++)
+            {
+                for (j =i+1; j < 5; j++)
+                {
+                    if (array[i] > array[j])
+                    {
+                        temp = array[i];
+                        array[i] = array[j];
+                        array[j] = temp;
+
+                    }
+                }
+            }
+
+            Console.WriteLine("Ascending order : ");
+            for (i = 0; i < 5; i++)
+            {
+                Console.WriteLine(array[i]);
+            }
+
+            Console.WriteLine("Desending order : ");
+            for (i = 4; i >= 0; i--)
+            {
+                Console.WriteLine(array[i]);
+            }
+
+        }
+    }
+}
---
+++ Switch_vowel_consonent/Switch_vowel_consonent/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+namespace Switch
+{
+    class program
+    {
+        static void Main(string[] args)
+        {
+            char ch;
+
+            Console.WriteLine("Enter any letter : ");
+            ch = Convert.ToChar(Console.ReadLine());
+
+            switch (Char.ToLower(ch))
+            {
+                case 'a':
+                case 'e':
+                case 'i':
+                case 'o':
+                case 'u':
+                    Console.WriteLine("Vowel\n");
+                    break;
+
+                default:
+                    Console.WriteLine("Consonent\n");
+                    break;
+            }
+
+
+
+        }
+    }
+}
